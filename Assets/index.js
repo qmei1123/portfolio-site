3179b3711df57e8440a04c8ed8d0d204108b1f6d
qmei1123 <qmei@bu.edu>
2018-04-15 19:31:47 -0400

    add processing pop-up

--- Assets/index.js
+++ Assets/index.js
@@ -5,9 +5,30 @@ $(".quotes").css("opacity", ".9");
 
 function quoteChange() {
     var randnum = Math.random();
-        var inum = 21;
+        var inum = 23;
         var rand1 = Math.round(randnum * (inum-1));
-        var quotes = new Array("Left index finger chopped off with butcher's knife at the age of 14 in failed attempt to make acorn squash soup.", "During the 2017 marathon season, only had a set of seven complete toenails. They stil haven't grown back.","No Asian Glow!", "Consumed four total Starbucks drinks: Java Chip, Java Chip, Java Chip. Java Chip. Tall.", "One Market Pantry jar of Natural Peanut Butter per six days.", "Sweat machine.", "Gym ratty.", "Spontaneous violin jam session with two street artists in Florence.", "Guess how to pronounce it. Q-I-A-N.", "Purple cabbage makes Blue Dumplings. A Venetian Classic.", "Olivia says Qian spent twelve dollars this fiscal year. False.", "Cracked robin eggs as a profession in Kindergarten.", "Oreos assisted in journey through Anna Karenina.", "True Story, Potential Headline: Alone in Paris for Six Days, All Meals Made from Supermarket Food.", "Sprained ankle in the rain, presented torturously rank feet to nurse for wrapping.", "Counts in Korean to work out, nothing else. Doesn't know Korean at all.", "Enjoyed lying on top of cars in motion until a Good Samaritan told us to get off before she called the Cops.", "Testing for Taekwondo Black Belt in five days. In need of serious moral support.", "Listened to 85,872 minutes of music in 2017.", "S");
+        var quotes = new Array("Left index finger chopped off with butcher's knife at the age of 14 in failed attempt to make acorn squash soup.", 
+                               "During the 2017 marathon season, only had a set of seven complete toenails. They still haven't grown back.",
+                               "No Asian Glow!", 
+                               "Consumed four total Starbucks drinks: Java Chip, Java Chip, Java Chip. Java Chip. Tall.", 
+                               "One Market Pantry jar of Natural Peanut Butter per six days.", 
+                               "Sweat machine.", 
+                               "Gym ratty.", 
+                               "Spontaneous violin jam session with two street artists in Florence.", 
+                               "Guess how to pronounce it. Q-I-A-N.", 
+                               "Purple cabbage makes Blue Dumplings. A Venetian Classic.", 
+                               "Olivia says Qian spent twelve dollars this fiscal year. False.", 
+                               "Cracked robin eggs as a profession in Kindergarten.", 
+                               "Forced to read Anna Karenina at age 10, could only read it with a pack of Oreos in front of me.", 
+                               "True Story, Potential Headline: Alone in Paris for Six Days, All Meals Made from Supermarket Food.", 
+                               "Sprained ankle in the rain, presented torturously rank feet to nurse for wrapping.",
+                               "Counts in Korean to work out, nothing else. Doesn't know Korean at all.", 
+                               "Enjoyed lying on top of cars in motion until a Good Samaritan told us to get off before she called the Cops.", 
+                               "Used two Micron pens in place of chopsticks to eat Flamin' Hot Cheetohs",
+                               "Listened to 85,872 minutes of music in 2017.", 
+                               "PR'ed at the 2017 Baystate Marathon after fainting at mile 25.5", 
+                               "Enjoys a cup of tomato juice, no ice, on every flight. Beverage of choice.", 
+                               "Customer once asked for a shot of Bailey's, received Bailey's, vodka, Kahlua, and cream.");
         var selectQuote = quotes[rand1];
         return selectQuote;
 };   
@@ -34,18 +55,20 @@ $(".col-sm-5").hover(function(){
     });
 
     $("#img-five").hover(function(){
-                        $(".quotes").text("Cake or Life");
+                        $(".quotes").text("Unus Mundus");
     });
     
     $("#img-six").hover(function(){
-                        $(".quotes").text("Collections");
+                        $(".quotes").text("Taekwondo");
     });
     
     $("#img-seven").hover(function(){
                         $(".quotes").text("Handy work");
     });
-    
     $("#img-eight").hover(function(){
+                        $(".quotes").text("Urban Case Studies");
+    });
+    $("#img-nine").hover(function(){
                         $(".quotes").text("Piqued your interest yet?");
     });
     
@@ -84,6 +107,10 @@ $("#img-seven").click(function()
 });
 $("#img-eight").click(function()
                  {$("#pop-seven").show(500);
+                  $("body").css("overflow-y", "hidden");
+});
+$("#img-nine").click(function()
+                 {$("#pop-eight").show(500);
                   $("body").css("overflow-y", "hidden");
 });
 
